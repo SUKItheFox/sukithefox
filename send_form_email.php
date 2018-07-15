a8f74c6e9c6a511167fef2468c9ab65ef1ece812
Suki <mari@to-bee.art>
2018-07-15 20:33:31 +0300

    Add form validation

--- send_form_email.php
+++ send_form_email.php
@@ -2,7 +2,7 @@
 if(isset($_POST['email'])) {
  
     // EDIT THE 2 LINES BELOW AS REQUIRED
-    $email_to = "mari.michelson@gmail.com";
+    $email_to = mari.michelson@gmail.com;
     $email_subject = "Your email subject line";
  
     function died($error) {
