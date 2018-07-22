e4b3fa061d8bde9e63383b5bdb996131c4b273ee
Suki <mari@to-bee.art>
2018-07-22 14:49:53 +0300

    update send_form_email.php

--- send_form_email.php
+++ send_form_email.php
@@ -32,7 +32,7 @@ $headers .= "Reply-To: $visitor_email \r\n";
 //Send the email!
 mail($to,$email_subject,$email_body,$headers);
 //done. redirect to thank-you page.
-header('Location: thank-you.html');
+echo "Thank you, you message has been sent!";
 
 
 // Function to validate against any email injection attempts
