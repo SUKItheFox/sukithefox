696ac2fdca2afd7d9f57b69d3da74cceca3215ed
Suki <mari.michelson@gmail.com>
2019-01-28 23:27:03 +0200

    Update email handler

--- send_form_email.php
+++ send_form_email.php
@@ -1,34 +1,33 @@
-
 <?php 
-  $name = $_POST['name'];
-  $sender_email = $_POST['email'];
-  $message = $_POST['message'];
+    $name = $_POST['name'];
+    $sender_email = $_POST['email'];
+    $message = $_POST['message'];
 
-  $email_from = "$sender_email"; 
-  $email_subject = "Inquiry to SUKItheFox";
-  $email_body = "User Name: $name.\n".
-          "User Email: $sender_email.\n".
-            "User Message: $message.\n";
+    $email_from = "$sender_email"; 
+    $email_subject = "Inquiry to SUKItheFox";
+    $email_body = "User Name: $name.\n".
+            "User Email: $sender_email.\n".
+              "User Message: $message.\n";
 
 
-  $email_to = "mari@to-bee.art";
-  
-  $headers = "From: $email_from \r\n";
+    $to = "mari@to-bee.art";
+    
+    $headers = "From: $email_from \r\n";
 
-  $headers .= "Reply-To: $sender_email \r\n";
+    $headers .= "Reply-To: $sender_email \r\n";
 
-  mail($email_to,$email_subject,$email_body,$headers);          
+    mail($to,$email_subject,$email_body,$headers);          
 
-  
+    
 
-  if (mail($email_to,$email_subject,$email_body,$headers)) { 
-    header("Refresh: 5; url=index.html");
-    echo 'Thank you for the message!';
-       
-    } else {
+    if (mail($to,$email_subject,$email_body,$headers)) { 
       header("Refresh: 5; url=index.html");
-    echo "Message Sending Failed, try again";
-    }
+      echo 'Thank you for the message!';
+         
+      } else {
+        header("Refresh: 5; url=index.html");
+      echo "Message Sending Failed, try again";
+      }
 
 
 
