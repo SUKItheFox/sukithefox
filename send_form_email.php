12c86405fc2da3d1facc5c7eea7518f48eb83906
Suki <mari.michelson@gmail.com>
2019-01-28 22:53:46 +0200

    Add favicon

--- send_form_email.php
+++ send_form_email.php
@@ -1,125 +1,35 @@
-<?php
- 
-  if(isset($_POST['email'])) {
-   
-       
-   
-      // EDIT THE 2 LINES BELOW AS REQUIRED
-   
-      $email_to = "mari@to-bee.art";
-   
-      $email_subject = "Inquiry to SUKItheFox";
-   
-       
-   
-       
-   
-      function died($error) {
-   
-          // your error code can go here
-   
-          echo "We are very sorry, but there were error(s) found with the form you submitted. ";
-   
-          echo "These errors appear below.<br /><br />";
-   
-          echo $error."<br /><br />";
-   
-          echo "Please go back and fix these errors.<br /><br />";
-   
-          die();
-   
-      }
-   
-       
-   
-      // validation expected data exists
-   
-      if(!isset($_POST['name']) ||
-   
-          !isset($_POST['email']) ||
-   
-          !isset($_POST['message'])) {
-   
-          died('We are sorry, but there appears to be a problem with the form you submitted.');       
-   
-      }
-   
-       
-   
-      $name = $_POST['name']; // required
-   
-      $email_from = $_POST['email']; // required
-   
-      $message = $_POST['message']; // required
-   
-       
-   
-      $error_message = "";
-   
-      $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
-   
-    if(!preg_match($email_exp,$email_from)) {
-   
-      $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
-   
-    }
-   
-      $string_exp = "/^[A-Za-z .'-]+$/";
-   
-    if(!preg_match($string_exp,$name)) {
-   
-      $error_message .= 'The Name you entered does not appear to be valid.<br />';
-   
-    }
-   
-   
-   
-    if(strlen($message) < 2) {
-   
-      $error_message .= 'The Message you entered do not appear to be valid.<br />';
-   
-    }
-   
-    if(strlen($error_message) > 0) {
-   
-      died($error_message);
-   
-    }
-   
-      $email_message = "Form details below.\n\n";
-   
-       
-   
-      function clean_string($string) {
-   
-        $bad = array("content-type","bcc:","to:","cc:","href");
-   
-        return str_replace($bad,"",$string);
-   
-      }
-   
-       
-   
-      $email_message .= "Name: ".clean_string($name)."\n";
-   
-      $email_message .= "Email: ".clean_string($email_from)."\n";
-   
-      $email_message .= "Message: ".clean_string($message)."\n";
-   
-       
-   
-       
-     
-    // create email headers
-     
-    $headers = 'From: '.$email_from."\r\n".
-     
-    'Reply-To: '.$email_from."\r\n" .
-     
-    'X-Mailer: PHP/' . phpversion();
-     
-    mail($email_to, $email_subject, $email_message, $headers);  
-  }  
-   
-?>
 
+<?php 
+  $name = $_POST['name'];
+  $email_from = $_POST['email'];
+  $message = $_POST['message'];
+
+  
+  $email_subject = "Inquiry to SUKItheFox";
+  $email_body = "User Name: $name.\n".
+          "User Email: $visitor_email.\n".
+            "User Message: $message.\n";
+
+
+  $email_to = "mari@to-bee.art";
+  
+  $headers = "From: $email_from \r\n";
+
+  $headers .= "Reply-To: $email_from \r\n";
+
+  mail($email_to,$email_subject,$email_body,$headers);          
+
+  
+
+  if (mail($email_to,$email_subject,$email_body,$headers)) { 
+    header("Refresh: 5; url=index.html");
+    echo 'Thank you for the message!';
+       
+    } else {
+      header("Refresh: 5; url=index.html");
+    echo "Message Sending Failed, try again";
+    }
+
+
+
+?>
