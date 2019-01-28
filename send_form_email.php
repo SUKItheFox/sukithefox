f70ae469dd9925756155106759e822659a777fc5
Suki <mari.michelson@gmail.com>
2019-01-28 23:59:19 +0200

    Update form handler email

--- send_form_email.php
+++ send_form_email.php
@@ -1,34 +1,47 @@
-<?php 
-  $name = $_POST['name'];
-  $sender_email = $_POST['email'];
-  $message = $_POST['message'];
-
-  $email_from = "$sender_email"; 
-  $email_subject = "Inquiry to SUKItheFox";
-  $email_body = "User Name: $name.\n".
-          "User Email: $sender_email.\n".
-            "User Message: $message.\n";
-
-
-  $to = "mari@to-bee.art";
-  
-  $headers = "From: $email_from \r\n";
-
-  $headers .= "Reply-To: $sender_email \r\n";
-
-  mail($to,$email_subject,$email_body,$headers);          
-
-  
-
-  if (mail($to,$email_subject,$email_body,$headers)) { 
-    header("Refresh: 5; url=index.html");
-    echo 'Thank you for the message!';
-       
-  } else {
-    header("Refresh: 5; url=index.html");
-    echo "Message Sending Failed, try again";
-  }
-
-
-
+<?php
+error_reporting(E_ERROR | E_WARNING | E_PARSE);
+ini_set('display_errors','On');
+$disabled = explode(',', ini_get('disable_functions'));
+if(in_array('mail', $disabled)) die("mail function is disabled on this server");
+$debug = true; //change value to false once the form is working
+$errors = '';
+$myemail = 'mari@to-bee.art';//<-----Put Your email address here.
+if(isset($_POST['name']))
+    {
+    if(empty($_POST['name'])  ||
+       empty($_POST['email']) ||
+       empty($_POST['message']))
+    {
+        $errors .= "\n Error: all fields are required";
+    }
+    if( empty($errors))
+    {
+        $name = $_POST['name'];
+        $email_address = $_POST['email'];
+        $message = $_POST['message'];
+        if (!preg_match("/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i",$email_address))
+        {
+            $errors .= "\n Error: Invalid email address";
+        }
+        if( empty($errors))
+        {
+            $to = $myemail;
+            $email_subject = "Contact form submission: $name";
+            $email_body = "You have received a new message. ".
+            " Here are the details:\n Name: $name \n Email: $email_address \n Message \n $message";
+            $headers = "From: $myemail\n";
+            $headers .= "Reply-To: $email_address";
+            $send = (function_exists('mail')) ? @mail($to,$email_subject,$email_body,$headers) : false;
+            if($send) {
+                //redirect to the 'thank you' page
+                header('Location: contact-form-thank-you.html');
+                exit;
+            } else {
+                if($debug) phpinfo();
+                else $errors .= 'Server error: Please report to the webmaster';
+                exit;    
+            }
+        }
+    }
+} //end isset name
 ?>
