0a8433465e33eba7dd0ce635acf75943126e5672
Suki <mari.michelson@gmail.com>
2019-02-12 17:03:50 +0200

    Update PHP form

--- send_form_email.php
+++ send_form_email.php
@@ -1,47 +1,41 @@
 <?php
-error_reporting(E_ERROR | E_WARNING | E_PARSE);
-ini_set('display_errors','On');
-$disabled = explode(',', ini_get('disable_functions'));
-if(in_array('mail', $disabled)) die("mail function is disabled on this server");
-$debug = true; //change value to false once the form is working
-$errors = '';
-$myemail = 'mari@to-bee.art';//<-----Put Your email address here.
-if(isset($_POST['name']))
-    {
-    if(empty($_POST['name'])  ||
-       empty($_POST['email']) ||
-       empty($_POST['message']))
-    {
-        $errors .= "\n Error: all fields are required";
+
+if(isset($_POST['submit'])){
+
+    require 'PHPMailer/PHPMailerAutoload.php';
+
+    // Send mail
+    $mail = new PHPMailer();
+
+    // Data received from POST request
+    $name = stripcslashes($_POST['tbName']);
+    $email = stripcslashes($_POST['tbEmail']);
+    $message = stripcslashes($_POST['taMessage']); 
+
+    // SMTP Configuration
+    $mail->SMTPAuth = true; 
+    $mail->IsSMTP();
+    $mail->Host = "mail.infomaniak.com"; // SMTP server
+    $mail->Username = "mari@to-bee.art";
+    $mail->Password = "Valgus9Armastus8";
+    $mail->SMTPSecure = 'tls';   
+    $mail->Port = 587; 
+
+    $mail->AddAddress('mari@to-bee.art');
+    $mail->From = "mari@to-bee.art";
+    $mail->FromName = "Website Contact Form - " . $name;
+   
+
+    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';    
+    $mail->AltBody = "To view the message, please use an HTML compatible email viewer!"; // optional, comment out and test
+    $mail->MsgHTML("Name:" . $name . "<br /><br />Email:" . $email. "<br /><br />Company:" . $message. "<br /><br />Subject:" . $message. "<br /><br />" . $message);
+
+    $message = NULL;
+    if(!$mail->Send()) {
+        $message = "Mailer Error: " . $mail->ErrorInfo;
+    } else {
+        $message = "Message sent!";
     }
-    if( empty($errors))
-    {
-        $name = $_POST['name'];
-        $email_address = $_POST['email'];
-        $message = $_POST['message'];
-        if (!preg_match("/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i",$email_address))
-        {
-            $errors .= "\n Error: Invalid email address";
-        }
-        if( empty($errors))
-        {
-            $to = $myemail;
-            $email_subject = "Contact form submission: $name";
-            $email_body = "You have received a new message. ".
-            " Here are the details:\n Name: $name \n Email: $email_address \n Message \n $message";
-            $headers = "From: $myemail\n";
-            $headers .= "Reply-To: $email_address";
-            $send = (function_exists('mail')) ? @mail($to,$email_subject,$email_body,$headers) : false;
-            if($send) {
-                //redirect to the 'thank you' page
-                header('Location: contact-form-thank-you.html');
-                exit;
-            } else {
-                if($debug) phpinfo();
-                else $errors .= 'Server error: Please report to the webmaster';
-                exit;    
-            }
-        }
-    }
-} //end isset name
+
+}
 ?>
