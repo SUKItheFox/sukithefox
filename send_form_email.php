5b346ff2de9d53f96eeb98934f3c0cc8002e9384
Suki <mari.michelson@gmail.com>
2019-02-12 18:49:26 +0200

    Update PHP contact form to sendgrid

--- send_form_email.php
+++ send_form_email.php
@@ -1,42 +1,39 @@
 <?php
+if(isset($_POST['submit'])) 
+{
 
-if(isset($_POST['submit'])){
-
-    require 'PHPMailer/PHPMailerAutoload.php';
-
-    // Send mail
-    $mail = new PHPMailer();
-
-    // Data received from POST request
-    $name = stripcslashes($_POST['tbName']);
-    $email = stripcslashes($_POST['tbEmail']);
-    $message = stripcslashes($_POST['taMessage']); 
-
-    // SMTP Configuration
+$message=
+'Full Name: '.$_POST['name'].'<br />
+Email:  '.$_POST['email'].'<br />
+Message:    '.$_POST['message'].'<br />
+';
+    require "phpmailer/class.phpmailer.php"; //include phpmailer class
+      
+    // Instantiate Class  
+    $mail = new PHPMailer();  
+      
+    // Set up SMTP  
+    $mail->IsSMTP();                // Sets up a SMTP connection  
     $mail->SMTPAuth = true; 
-    $mail->IsSMTP();
-    $mail->Host = "mail.infomaniak.com"; // SMTP server
-    $mail->Username = ENV['EMAIL_USERNAME'];
-    $mail->Password = ENV['EMAIL_PASSWORD'];
+    $mail->Host = "smtp.sendgrid.net"; // SMTP server
     $mail->SMTPSecure = 'tls';   
     $mail->Port = 587; 
-   
-
-    $mail->AddAddress('mari@to-bee.art');
-    $mail->From = "mari@to-bee.art";
-    $mail->FromName = "Website Contact Form - " . $name;
-   
-
-    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';    
-    $mail->AltBody = "To view the message, please use an HTML compatible email viewer!"; // optional, comment out and test
-    $mail->MsgHTML("Name:" . $name . "<br /><br />Email:" . $email. "<br /><br />Company:" . $message. "<br /><br />Subject:" . $message. "<br /><br />" . $message);
-
-    $message = NULL;
-    if(!$mail->Send()) {
-        $message = "Mailer Error: " . $mail->ErrorInfo;
-    } else {
-        $message = "Message sent!";
-    }
+    
+    // Authentication  
+    $mail->Username = "apikey";
+    $mail->Password = "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";
+      
+    // Compose
+    $mail->SetFrom($_POST['email'], $_POST['name']);
+    $mail->AddReplyTo($_POST['email'], $_POST['name']);
+    $mail->Subject = "New Contact Form Enquiry";      // Subject (which isn't required)  
+    $mail->MsgHTML($messages);
+ 
+    // Send To  
+    $mail->AddAddress("mari.michelson@gmail.com", "Recipient Name"); // Where to send it - Recipient
+    $result = $mail->Send();        // Send!  
+    $messages = $result ? 'Successfully Sent!' : 'Sending Failed!';      
+    unset($mail);
 
 }
 ?>
