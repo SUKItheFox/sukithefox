77a9f2812de7d26537417067c365d71889b91145
Suki <mari.michelson@gmail.com>
2019-02-12 19:30:26 +0200

    update contact form

--- send_form_email.php
+++ send_form_email.php
@@ -1,11 +1,12 @@
 <?php 
 require_once('sendgrid-php/sendgrid-php.php');
 
-$from = new SendGrid\Email(null, "mari.michelson@gmail.com");
-$subject = "Hello World from the SendGrid PHP Library!";
-$to = new SendGrid\Email(null, "mari@to-bee.art");
-$message = new SendGrid\Content("text/plain", "Hello, Email!");
-$mail = new SendGrid\Mail($from, $subject, $to, $message);
+$from = new SendGrid\Email(null, $_POST['email']);
+$name = new SendGrid\fname(null, $_POST['name']); //senders fname
+$subject = "Email from SUKItheFox Website";
+$to = new SendGrid\Email(null, "mari.michelson@gmail.com");
+$message = new SendGrid\Content(null, $_POST['message']);
+$mail = new SendGrid\Mail($from, $name, $subject, $to, $message);
 
 $apiKey = (getenv('SENDGRID_API_KEY'));
 $sg = new \SendGrid($apiKey);
