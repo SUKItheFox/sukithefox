219d295f0dfa4f1356edd6b630b1dddd256a67b5
Suki <mari.michelson@gmail.com>
2019-02-12 19:16:24 +0200

    Update sendgrid details

--- send_form_email.php
+++ send_form_email.php
@@ -1,39 +1,19 @@
-<?php
-if(isset($_POST['submit'])) 
-{
+<?php 
+require_once('sendgrid-php/sendgrid-php.php');
 
-$message=
-'Full Name: '.$_POST['name'].'<br />
-Email:  '.$_POST['email'].'<br />
-Message:    '.$_POST['message'].'<br />
-';
-    require "phpmailer/class.phpmailer.php"; //include phpmailer class
-      
-    // Instantiate Class  
-    $mail = new PHPMailer();  
-      
-    // Set up SMTP  
-    $mail->IsSMTP();                // Sets up a SMTP connection  
-    $mail->SMTPAuth = true; 
-    $mail->Host = "smtp.sendgrid.net"; // SMTP server
-    $mail->SMTPSecure = 'tls';   
-    $mail->Port = 587; 
-    
-    // Authentication  
-    $mail->Username = "apikey";
-    $mail->Password = "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";
-      
-    // Compose
-    $mail->SetFrom($_POST['email'], $_POST['name']);
-    $mail->AddReplyTo($_POST['email'], $_POST['name']);
-    $mail->Subject = "New Contact Form Enquiry";      // Subject (which isn't required)  
-    $mail->MsgHTML($messages);
- 
-    // Send To  
-    $mail->AddAddress("mari.michelson@gmail.com", "Recipient Name"); // Where to send it - Recipient
-    $result = $mail->Send();        // Send!  
-    $messages = $result ? 'Successfully Sent!' : 'Sending Failed!';      
-    unset($mail);
+$from = new SendGrid\Email(null, "mari.michelson@gmail.com");
+$subject = "Hello World from the SendGrid PHP Library!";
+$to = new SendGrid\Email(null, "mari@to-bee.art");
+$message = new SendGrid\Content("text/plain", "Hello, Email!");
+$mail = new SendGrid\Mail($from, $subject, $to, $message);
 
+$apiKey = (getenv('SENDGRID_API_KEY'));
+$sg = new \SendGrid($apiKey);
+
+$response = $sg->client->mail()->send()->post($mail);
+if($response->statusCode() == 202){
+    echo "Email sent successfully";
+}else{
+    echo "Email could not be sent";
 }
 ?>
